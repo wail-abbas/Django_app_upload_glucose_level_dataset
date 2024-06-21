1cd98bc6328b3e194b65a6bf3ddfdff14224eb96
wail-abbas <wailabbas.ahmed@gmail.com>
2024-06-21 11:40:26 +0200

    add user_abbreviation field

--- core_app/models.py
+++ core_app/models.py
@@ -3,4 +3,5 @@ from django.db import models
 
 # Create your models here.
 class User(AbstractUser):
+  user_abbreviation = models.CharField(max_length=50, blank = False, unique = True) 
   email = models.EmailField(unique=True)
