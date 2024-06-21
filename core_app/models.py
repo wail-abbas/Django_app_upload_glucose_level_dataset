d1cceff68dcb5a0db42d3264b703187034b5e420
wail-abbas <wailabbas.ahmed@gmail.com>
2024-06-21 12:03:28 +0200

    add models to the devices_app

--- core_app/models.py
+++ core_app/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class User(AbstractUser):
   user_abbreviation = models.CharField(max_length=50, blank = False, unique = True) 
   email = models.EmailField(unique=True)
-  address = models.CharField(max_length=50, blank = False, unique = False)
-  birthdate = models.CharField(max_length=50, blank = False, unique = False)
-  mobile_number = models.CharField(max_length=20, blank = False, unique = True)
+  address = models.CharField(max_length=50, blank = True, unique = False)
+  birthdate = models.CharField(max_length=50, blank = True, unique = False)
+  mobile_number = models.CharField(max_length=50, blank = True, unique = False)
   user_abbreviation = models.CharField(max_length=50, blank = False, unique = True)
--- devices_app/models.py
+++ devices_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from core_app.models import User
 
 # Create your models here.
+class Devices(models.Model):
+    device_type = models.CharField(max_length=255)
+
+
+class UserDevice(models.Model):
+    serial_number = models.CharField(max_length=150, blank = False, unique = True)
+    device_type = models.ForeignKey(Devices, on_delete=models.PROTECT, related_name='devices')
+    user_id = models.ForeignKey(User, on_delete=models.PROTECT, related_name='users')
