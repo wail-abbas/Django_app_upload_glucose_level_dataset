05cba49ff92032ddd37c8d032ef7dd90cf47aff5
wail-abbas <wailabbas.ahmed@gmail.com>
2024-06-21 12:10:40 +0200

    register models in admin.py

--- devices_app/admin.py
+++ devices_app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Devices, UserDevice
 
 # Register your models here.
+admin.site.register(Devices)
+admin.site.register(UserDevice)
--- devices_app/models.py
+++ devices_app/models.py
@@ -5,8 +5,14 @@ from core_app.models import User
 class Devices(models.Model):
     device_type = models.CharField(max_length=255)
 
+    def __str__(self) -> str:
+        return self.device_type
+
 
 class UserDevice(models.Model):
     serial_number = models.CharField(max_length=150, blank = False, unique = True)
     device_type = models.ForeignKey(Devices, on_delete=models.PROTECT, related_name='devices')
     user_id = models.ForeignKey(User, on_delete=models.PROTECT, related_name='users')
+    
+    def __str__(self) -> str:
+        return self.serial_number
